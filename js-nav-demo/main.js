aab8c87892d1b24e6d9bddb91d7fb93a2c3a7b82
zhang <237584904@qq.com>
2018-04-09 13:24:14 +0800

    js-nav-version_1

--- js-nav-demo/main.js
+++ js-nav-demo/main.js
@@ -1,19 +1,35 @@
-var keyboard =  {
+var init = init();
+
+var keyboard = init.keyboard;
+
+var hash = init.hash; 
+
+
+generateKeyboard(keyboard,hash);
+
+listenEvents(hash);
+
+function init(){
+	var keyboard =  {
 				'0': {0:'q',1:'w',2:'e',3:'r',4:'t',5:'y',6:'u',7:'i',8:'o',9:'p',length:10},
 				'1': {0:'a',1:'s',2:'d',3:'f',4:'g',5:'h',6:'j',7:'k',8:'l',length:9},
 				'2': {0:'z',1:'x',2:'c',3:'v',4:'b',5:'n',6:'m',length:7},
 				'length': 3
 			}
 
-var hash = {
-	'q':'qq.com','b':'bilibili.com','t':'taobao.com','z':'zhihu.com','d':'didiaokan.com','w':'weibo.com'
+	var hash = {
+		'q':'qq.com','b':'bilibili.com','t':'taobao.com','z':'zhihu.com','d':'didiaokan.com','w':'weibo.com'
+			}	
+
+	var hashInlocalStorage = JSON.parse(localStorage.getItem('websites') || 'null');
+	if (hashInlocalStorage) {
+			hash = hashInlocalStorage;
+		}
+	return {
+		hash: hash,
+		keyboard : keyboard
+			}
 }
-
-
-
-generateKeyboard(keyboard,hash);
-
-listenEvents(hash);
 
 function listenEvents(hash){
 	document.onkeypress = function(keyDown){
@@ -21,10 +37,10 @@ function listenEvents(hash){
 	//	var whichButton = document.querySelector(`button[id="${key}"]`);
 	//	var whichKey = whichButton.parentNode;
 	//	whichKey.classList.add('playing');
-		var hashInlocalStorage = JSON.parse(localStorage.getItem('websites') || 'null');
-		if (hashInlocalStorage) {
-			hash = hashInlocalStorage;
-		}
+		// var hashInlocalStorage = JSON.parse(localStorage.getItem('websites') || 'null');
+		// if (hashInlocalStorage) {
+		// 	hash = hashInlocalStorage;
+		// }
 		var website = hash[key];
 
 		if (website!= undefined) {
@@ -47,9 +63,11 @@ function generateKeyboard(keyboard,hash){
 		for (var j = 0; j < row.length; j++) {
 			var span = createSpan(row[j]);
 			var button = createButton(row[j]);
+			var img = createImg(row[j]);
 			var kbd = document.createElement('kbd');
 			kbd.className = 'sound';
 			kbd.appendChild(button);
+			kbd.appendChild(img);
 			kbd.appendChild(span);
 			div.appendChild(kbd);	
 
@@ -57,7 +75,18 @@ function generateKeyboard(keyboard,hash){
 		}
 	}
 
+	console.log("keyboard");
 
+}
+
+function createImg(content){
+	var img = document.createElement('img');
+	if (hash[content]!= undefined) {
+	img.src = 'http://'+hash[content]+'/favicon.ico';
+	console.log(hash[content]);
+	}
+	img.className = 'icon';
+	return img;
 }
 
 function createSpan(content){
@@ -76,6 +105,8 @@ function createButton(content){
 		var buttonDown = event.target;
 		var key = buttonDown.id;
 		var x = prompt('give a website');
+		var img = buttonDown.nextElementSibling;
+		img.src = 'http://'+x+'/favicon.ico'; 
 		hash[key] =  x;
 		localStorage.setItem('websites',JSON.stringify(hash));
 		};
