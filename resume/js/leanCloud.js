08f3996e22228a5207b67990207748a2f7c98e6f
zhang <237584904@qq.com>
2018-05-25 20:20:24 +0800

    留言功能

--- resume/js/leanCloud.js
+++ resume/js/leanCloud.js
@@ -6,11 +6,43 @@ AV.init({
   appKey: APP_KEY
 });
 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
+var myForm = document.querySelector('#postMessageForm');//监听表单，能够满足点击跟回车同时。
+myForm.addEventListener('submit', function (e) {
+  e.preventDefault(); //阻止默认事件
+  let name = myForm.querySelector('input[name=name]').value;
+  let content = myForm.querySelector('input[name=content]').value;
+  var Message = AV.Object.extend('Message');
+  var message = new Message();
+  message.save({
+    'name': name,
+    'content': content
+  }).then(function (object) {
+    let li = document.createElement('li');
+    li.innerText = `${object.attributes.name}:   ${object.attributes.content}`;
+    let messageList = document.querySelector('#messages > ol')
+    messageList.appendChild(li); 
+  })
 })
+
+var query = new AV.Query('Message');
+query.find().then(function (messages) {
+    let array = messages.map((item)=>item.attributes)
+    array.forEach((item) => {
+      let li = document.createElement('li');
+      li.innerText = item.content;
+      let messageList = document.querySelector('#messages > ol')
+      messageList.appendChild(li);
+    });
+}).then(function(todos) {
+  // 更新成功
+}, function (error) {
+  // 异常处理
+});
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'Hello World!'
+// }).then(function(object) {
+//   alert('LeanCloud Rocks!');
+// })
 
